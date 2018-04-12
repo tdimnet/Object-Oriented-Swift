9927b5a2b51350b29bcbc4e5bfe5cab613e0340b
tdimnet <thomas.dimnet@gmail.com>
2018-04-12 14:03:27 +0200

    Add construct final challenge.

--- Object-Oriented-Swift/main.swift
+++ Object-Oriented-Swift/main.swift
@@ -90,10 +90,28 @@ let myFullName: String = aPerson.fullName()
 print(myFullName)
 
 
+/*
+ RGBColor Struct
+ */
+struct RGBColor {
+    let red: Double
+    let green: Double
+    let blue: Double
+    let alpha: Double
+    
+    let description: String
+    
+    init(red: Double, green: Double, blue: Double, alpha: Double) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+        self.description = "red: \(red), green: \(green), blue: \(blue), alpha: \(alpha)"
+    }
+}
 
-
-
-
+let firstRGBColor = RGBColor(red: 86.0, green: 191.0, blue: 131.0, alpha: 1.0)
+print(firstRGBColor.description)
 
 
 
